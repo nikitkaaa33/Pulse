7213814f125b947a079008f2d776a023ce919691
nikita <nikitkacool29@gmail.com>
2022-09-17 21:51:40 +0300

    add new gulfgile, new dist

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'pulsepulse2022@gmail.com';                 // Наш логин
-$mail->Password = 'mailerpulse';                           // Наш пароль от ящика
+$mail->Password = 'ozthyqjrllswhpqb';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
